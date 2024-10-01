3eade292d8a7494c06c04ed18a018d7c87c8774c
George Oastler <goastler4@gmail.com>
2024-10-01 17:28:36 +0100

    fix webview console log listeners + other methods

--- app/src/main/java/io/prosopo/procaptcha/MainActivity.kt
+++ app/src/main/java/io/prosopo/procaptcha/MainActivity.kt
@@ -1,58 +1,110 @@
 package io.prosopo.procaptcha
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Message
 import android.util.Base64
 import android.util.Log
+import android.webkit.ConsoleMessage
+import android.webkit.JsResult
+import android.webkit.PermissionRequest
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+const val namespace = "procaptcha";
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        // TODO don't forget to put key here
-        private const val SITE_KEY = ""
-    }
-
     private class ProcaptchaWebViewClient : WebViewClient() {
-
-        // TODO impl methods
-
-        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-            return false;
+        override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
+            // do nothing, don't worry about visited links
         }
+
+        override fun onFormResubmission(view: WebView, dontResend: Message, resend: Message) {
+            // just resend the message, don't bother the user
+            resend.sendToTarget()
+        }
+
         override fun onReceivedError(
-            view: WebView,
-            request: WebResourceRequest,
-            error: WebResourceError
+            view: WebView?,
+            request: WebResourceRequest?,
+            error: WebResourceError?
         ) {
-            Log.e("error", error.toString());
+            // TODO this is triggered whenever a resource can't load, bundle/img/whatever etc. Handle the type and display corresponding error
+            // handle http + ssl errors here to maintain compatibility with old android versions
+            super.onReceivedError(view, request, error)
         }
 
-        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-            super.onPageStarted(view, url, favicon)
-            Log.i("onPageStarted", url.toString());
-        }
-
-        override fun onPageFinished(view: WebView?, url: String?) {
-            super.onPageFinished(view, url)
-            Log.i("onPageFinished", url.toString());
-        }
-
-        override fun onLoadResource(view: WebView?, url: String?) {
-            super.onLoadResource(view, url)
-//        Log.i("onLoadResource", url.toString());
+        override fun shouldOverrideUrlLoading(
+            view: WebView,
+            request: WebResourceRequest
+        ): Boolean {
+            Log.i(namespace, "opening external url: ${request.url}")
+            // Open URLs in external browser
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(request.url.toString()))
+            view.context.startActivity(intent)
+            return true
         }
 
     }
 
     private class ProcaptchaWebChromeClient : WebChromeClient() {
-        // TODO impl methods
+
+        override fun getVisitedHistory(callback: ValueCallback<Array<String>>) {
+            // call the callback with empty history, making all links appear unvisited
+            callback.onReceiveValue(emptyArray())
+        }
+
+        override fun onCloseWindow(window: WebView) {
+            // don't close any windows associated with the webview
+        }
+
+        override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+            val message = "File: ${consoleMessage.sourceId()}\nLine: ${consoleMessage.lineNumber()}\nMessage: ${consoleMessage.message()}"
+            when (consoleMessage.messageLevel()) {
+                ConsoleMessage.MessageLevel.ERROR -> Log.e(
+                    namespace,
+                    message
+                )
+                ConsoleMessage.MessageLevel.WARNING -> Log.w(
+                    namespace,
+                    message
+                    )
+                ConsoleMessage.MessageLevel.DEBUG -> Log.d(
+                    namespace,
+                    message
+                )
+                else -> Log.i(namespace, message)
+            }
+            return true; // suppress default logging
+        }
+
+        override fun onCreateWindow(
+            view: WebView?,
+            isDialog: Boolean,
+            isUserGesture: Boolean,
+            resultMsg: Message?
+        ): Boolean {
+            val result = super.onCreateWindow(view, isDialog, isUserGesture, resultMsg)
+            if(!result) {
+                Log.e(namespace, "webview creation failed")
+            }
+            return result
+        }
+
+        override fun onPermissionRequest(request: PermissionRequest?) {
+            // TODO handle permissions
+            super.onPermissionRequest(request)
+        }
     }
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -77,14 +129,16 @@ class MainActivity : AppCompatActivity() {
         val html = """
             <html>
                 <head>
-                    <title>Procaptcha Demo</title>
+                    <title>Procaptcha</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
                     <script type="module" src="https://js.prosopo.io/js/procaptcha.bundle.js" async defer></script>
                 </head>
                 <body>
                     <form action="" method="POST">
+                        <a href="https://prosopo.io">Link</a>
                         <input type="text" name="email" placeholder="Email" />
                         <input type="password" name="password" placeholder="Password" />
-                        <div class="procaptcha" data-sitekey="${MainActivity.Companion.SITE_KEY}"></div>
+                        <div class="procaptcha" data-sitekey="5FWCbfR7pH9QiZqLgmm5Rw4QbFwyU5EaMqUV4G6xrvrTZDtC"></div>
                         <br />
                         <input type="submit" value="Submit" />
                     </form>
